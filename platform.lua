630cd0feb0a0ec3ba2dada55d000a7db8dda8ccb
pgmatev <petargabriel.matev@gmail.com>
2022-07-16 19:50:52 +0300

    platform assets

--- platform.lua
+++ platform.lua
@@ -2,11 +2,13 @@
 
 function createPlatform(world, x, y, image)
 	local platform = {}
+	local scalingX = love.graphics.getWidth() / 1920
+	local scalingY = love.graphics.getHeight() / 1080
 	platform.x = x * love.graphics.getWidth()
 	platform.y = y * love.graphics.getHeight()
 	platform.image = love.graphics.newImage(image)
-	platform.width = platform.image:getWidth()
-	platform.height = platform.image:getHeight()
+	platform.width = platform.image:getWidth() * scalingX
+	platform.height = platform.image:getHeight() * scalingY
 
 	--For collision recognition
 	platform.isPlatform = true
@@ -16,7 +18,7 @@ function createPlatform(world, x, y, image)
 	platform.draw = function ()
 		love.graphics.setColor(BASE_COLOR)
 		love.graphics.rectangle("fill", platform.x, platform.y, platform.width, platform.height)
-		love.graphics.draw(platform.image, platform.x, platform.y)
+		love.graphics.draw(platform.image, platform.x, platform.y, 0, scalingX, scalingY)
 	end
 
 	return platform
