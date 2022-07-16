513a1c10924ac970714e578b1c8ba2e59f211b5f
pgmatev <petargabriel.matev@gmail.com>
2022-07-16 13:35:15 +0300

    added dice value and character assets

--- dice.lua
+++ dice.lua
@@ -5,20 +5,44 @@ function CreateDice()
     Dice = {}
     Dice.x = 200
     Dice.y = 300
-    Dice.toggle = true
+    Dice.value = 1
+    Dice.isRolling = false
+
+    --Animation related
     Dice.spriteSheet = love.graphics.newImage("assets/dice/diceWhiteSprite.png")
     Dice.grid = anim8.newGrid(64, 64, Dice.spriteSheet:getWidth(), Dice.spriteSheet:getHeight())
     Dice.animation = anim8.newAnimation(Dice.grid('1-3', '1-2'), 0.2)
-    Dice.Draw = function ()
-        -- love.graphics.draw(Dice.image, Dice.x, Dice.y)
-        Dice.animation:draw(Dice.spriteSheet, Dice.x, Dice.y, nil)
+
+    --Methods
+    Dice.getRandomDice = function ()
+        return love.math.random(6)
     end
-    Dice.isRolling = false
     Dice.startRolling = function ()
-        Dice.isRolling = true
+        Dice.isRolling = true --Dice animation activates
         tick.delay(function ()
-            Dice.isRolling = false
+            Dice.isRolling = false --Dice animation stops
         end, 2)
+        local number = Dice.getRandomDice()
+        Dice.animation:gotoFrame(number)
+        if number == 1 then --The frame in the spreadsheet does not correspond to the value
+            Dice.value = 5
+        elseif number == 2 then
+            Dice.value = 3
+        elseif number == 3 then
+            Dice.value = 4
+        elseif number == 4 then
+            Dice.value = 6
+        elseif number == 5 then
+            Dice.value = 1
+        elseif number == 6 then
+            Dice.value = 2
+        end
+        print(Dice.value)
+    end
+    
+    --Default functions
+    Dice.Draw = function ()
+        Dice.animation:draw(Dice.spriteSheet, Dice.x, Dice.y, nil)
     end
     Dice.Update = function (dt)
         if Dice.isRolling then
