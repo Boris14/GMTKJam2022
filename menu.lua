24c868cc9cb8608068748b505cb855df15706efe
Boris Dimitrov <boris1234dimitrov@gmail.com>
2022-07-17 21:22:08 +0300

    Last

--- menu.lua
+++ menu.lua
@@ -22,6 +22,8 @@ function createMenu()
 		end
         love.graphics.setColor(PLAYER_COLORS[1])
         love.graphics.printf("RUN FOR THE DICE", 0.25 * love.graphics.getWidth(), 0.2 * love.graphics.getHeight(), love.graphics.getWidth()/2, "center")
+        love.graphics.printf("Player 1 controls: WASD - Move, T - Grab die, LShift - PowerUp", 0.1 * love.graphics.getWidth(), 0.4 * love.graphics.getHeight(), love.graphics.getWidth()/1.6, "left", 0, .5, .5)
+        love.graphics.printf("Player 2 controls: Arrows - Move, Space - Grab die, RShift - PowerUp", 0.6 * love.graphics.getWidth(), 0.4 * love.graphics.getHeight(), love.graphics.getWidth()/1.5, "right", 0, .5, .5)
         love.graphics.printf("Press SPACE to play", 0.35 * love.graphics.getWidth(), 0.6 * love.graphics.getHeight(), love.graphics.getWidth()/2, "center", 0, .5, .5)
         love.graphics.setColor(BASE_COLOR)
     end
